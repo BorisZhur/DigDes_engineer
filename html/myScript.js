a0514b29dfb2ea423bceb290252d3204c5dcede8
BorisZhur <50907618+BorisZhur@users.noreply.github.com>
2020-07-09 16:41:24 +0300

    Update myScript.js

--- html/myScript.js
+++ html/myScript.js
@@ -1,6 +1,10 @@
+
+
+
 function myScriptFunc (){
 	var x = new XMLHttpRequest();
 	x.open("GET", "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=6385d1369c67c03f03328a1a63a2f5a1&units=metric", true);
+
 	
 		x.onload = function myGet(){
 			
@@ -8,21 +12,18 @@ function myScriptFunc (){
 			var data = JSON.parse(x.responseText)
 			//document.write(typeof data)
 			var temp = document.getElementById("temp")
-			if ((x.status >= 200)&&(x.status<=202)) {
-				temp.innerHTML = data.main.temp
-			}
-			else
+			
+			if ((x.status!=200)&&(x.status!=201)&&(x.status!=202))
 			{
 				temp.innerHTML = "error"
 			}
-			
+			else
+			{
+				temp.innerHTML = data.main.temp
+			}				
 			
 		}
-	if ((x.status!=200)&&(x.status!=201)&&(x.status!=202))
-	{
-		var temp = document.getElementById("temp")
-		temp.innerHTML = "error"
-	}
+	
 	
 	x.send();
 }
@@ -37,20 +38,16 @@ function myScriptFuncOnClick(){
 			var data = JSON.parse(y.responseText)
 			//document.write(typeof data)
 			var temp = document.getElementById("temp")
-			if ((y.status >= 200)&&(y.status<=202)) {
-				temp.innerHTML = data.main.temp
-			}
-			else
+			
+			if ((y.status!=200)&&(y.status!=201)&&(y.status!=202))
 			{
 				temp.innerHTML = "error"
 			}
-			
+			else
+			{
+				temp.innerHTML = data.main.temp
+			}				
 			
 		}
-	if ((y.status!=200)&&(y.status!=201)&&(y.status!=202))
-	{
-		var temp = document.getElementById("temp")
-		temp.innerHTML = "error"
-	}	
 	y.send();
 }
